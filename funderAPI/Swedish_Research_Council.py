7bcfc94847f474f9308480d8ce6d3f519a16db26
FrankYiquan <147743539+FrankYiquan@users.noreply.github.com>
2025-08-06 14:04:35 +0800

    add Swedish Research Council API

---
+++ funderAPI/Swedish_Research_Council.py
@@ -0,0 +1,32 @@
+import requests
+
+def extract_Swedish_Research_Council_grant(projectId, apiKey = "Bearer VRSwecrisAPI2025-1"):
+    
+    normalized_projectId = projectId.split("VR")[1].strip() + "_VR" if projectId.startswith("VR") else projectId
+    final_projectId = normalized_projectId + "_VR" if not normalized_projectId.endswith("_VR") else normalized_projectId
+
+    url = f"https://swecris-api.vr.se/v1/projects/{final_projectId}"
+
+    headers = {
+    "Authorization": apiKey
+    }
+
+    response = requests.get(url, headers=headers)
+
+    startDate = None
+    amount = None 
+    if response.status_code == 200 and response != None:
+        data = response.json()
+        startDate = data.get("projectStartDate").split(" ")[0].strip()
+        amount = data.get("fundingsSek")
+    
+
+    return {
+        "startDate": startDate,
+        "amount": amount,
+        "currency": "kr"
+    }
+
+
+# print(extract_Swedish_Research_Council_grant("2021-03651"))
+
