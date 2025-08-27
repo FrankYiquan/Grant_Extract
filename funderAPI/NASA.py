20e724321efead157329a8d72cb7165b61f950b6
FrankYiquan <147743539+FrankYiquan@users.noreply.github.com>
2025-08-27 13:24:01 -0400

    merge Air Force Office of Scientific Research API into NASA API

--- funderAPI/NASA.py
+++ funderAPI/NASA.py
@@ -3,12 +3,16 @@ import requests
 def extract_NASA_grant(projectId):
     url = "https://api.usaspending.gov/api/v2/search/spending_by_award/"
 
+    #normalize projectId
+    clean_projectId = projectId.replace("-", "")
+
+
     payload ={
         "filters": {
-            "keywords": [projectId],
+            "keywords": [clean_projectId],
             "award_type_codes": ["02", "03", "04", "05"]
         },
-        "fields": ["Award ID", "Recipient Name", "Award Amount", "Start Date", "End Date"],
+        "fields": ["Award ID", "Recipient Name", "Award Amount", "Start Date", "End Date","cfda_program_title"],
         "limit": 1
     }
 
@@ -16,14 +20,16 @@ def extract_NASA_grant(projectId):
 
     amount = None
     startDate = None
-    if response.status_code == 200 and response:
+    if response.status_code == 200 and response and response.json().get("results"):
         data = response.json()
 
         grant = data.get("results")[0]
         amount = grant.get("Award Amount")
         startDate = grant.get("Start Date")
+        title = grant.get("cfda_program_title")
 
     return {
+        "title": title,
         "startDate": startDate,
         "amount": amount,
         "currency": "USD"
@@ -32,3 +38,5 @@ def extract_NASA_grant(projectId):
 
 
 print(extract_NASA_grant("80NSSC23K1596"))
+# grantId= "FA9550-23-1-0072"
+# print(extract_NASA_grant(grantId))
