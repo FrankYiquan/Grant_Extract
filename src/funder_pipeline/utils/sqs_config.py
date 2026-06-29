56f475b422f04718c2079f0ddcb8d3690e4e95ed
FrankYiquan <147743539+FrankYiquan@users.noreply.github.com>
2026-06-28 22:30:00 -0400

    Restore sqs config file

---
+++ src/funder_pipeline/utils/sqs_config.py
@@ -0,0 +1,11 @@
+# All SQS queue URLs for the funder_api pipeline
+
+NIH_QUEUE_URL = "https://sqs.us-east-2.amazonaws.com/050752631001/NIH-Queue"
+NSF_QUEUE_URL = "https://sqs.us-east-2.amazonaws.com/050752631001/NSF-Queue"
+EUROPE_RESEARCH_COUNCIL_QUEUE_URL = "https://sqs.us-east-2.amazonaws.com/050752631001/Europoean_Commission-Queue"
+
+# API_key for ExLibris API
+PRODUCTION_EXLIBRIS_API = ""
+
+# sanbbox
+SANDBOX_EXLIBRIS_API = "" 
